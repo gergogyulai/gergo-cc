e59bf11938541f536d109379989c11ab620f105a
Gergo <113334790+gergogyulai@users.noreply.github.com>
2023-05-13 01:07:18 +0200

    goofy ahhh typos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -38,8 +38,8 @@ export default function Home() {
         <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
 
           <h1 className='text-5xl font-bold'>Gergo's demo page</h1>
-          <p className='mt-4 text-xl'>Only old stuff here, nothing new i cloud put here</p>
-          <p className='mt-1'>I mainly work with Svelte(kit) and Next.js lately.</p>
+          <p className='mt-4 text-xl'>Only old stuff, nothing new I could put here</p>
+          <p className='mt-1'>I mainly work with Svelte(kit) and Next.js using Firebase lately.</p>
 
           <div className="mt-8 flex flex-col">
             <a
